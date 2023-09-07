bfb5325df5fa2ca37e2c64d1216e443a252dcfa9
O. Sharpe <osharpe001@gmail.com>
2023-09-07 14:57:47 -0400

    created middleware for authorization and errors. Created routing files.

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,33 @@
+const webToken = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("..models/userModel");
+
+const protect = asyncHandler(async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        try {
+            // TAKE TOKEN FROM THE HEADER
+            token = req.headers.authorization.split(" ")[1];
+
+            // VERIFY THE TOKEN
+            const decoded = webToken.verify(token, process.env.JWT_SECRET);
+
+            // GET USER FROM TOKEN
+            req. user = await User.findById(decoded.id).select("-password");
+
+            next();
+        } catch (err) {
+            console.log("LATEST ERROR FROM AUTHMIDDLEWARE: ", err);
+            res.status(401);
+            throw new Error("Not Authorized...");
+        };
+    };
+
+    if (!token) {
+        res.status(401);
+        throw new Error("Not Authorized (no credentials)...");
+    };
+});
+
+module.exports = { protect };
--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,9 @@ const cors = require("cors");
 const PORT = process.env.PORT;
 
 const app = express();
+
+// MIDDLEWARE-- //
+// TO ALLOW THE BODY OF A POST REQUEST
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
