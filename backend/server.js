9f0237d8d7625eb07baa31192b5a55b7b7096715
O. Sharpe <osharpe001@gmail.com>
2023-09-08 12:00:04 -0400

    finished creating server file

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,33 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const PORT = process.env.PORT;
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/database");
 
 const app = express();
+
+connectDB();
 
 // MIDDLEWARE-- //
 // TO ALLOW THE BODY OF A POST REQUEST
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// USE ROUTES/USERROUTES TO HANDLE ANY ENDPOINTS THAT END WITH /API/USERS
+app.use("/api/users", require("./routes/userRoutes"));
+// USE ROUTES/BLOGROUTES TO HANDLE ANY ENDPOINTS THAT END WITH /API/BLOGS
+app.use("/api/blogs", require("./routes/blogRoutes"));
+// USE ROUTES/COMMENTROUTES TO HANDLE ANY ENDPOINTS THAT END WITH /API/COMMENTS
+app.use("/api/comments", require("./routes/commentRoutes"));
+// USE ROUTES/LIKESROUTES TO HANDLE ANY ENDPOINTS THAT END WITH /API/LIKES
+app.use("/api/likes", require("./routes/likesRoutes"));
 
+// TRYING TO AVOID THE ACCESS-CONTROL-ALLOW-ORIGIN ISSUE
+app.use(cors());
+
+// HANDLING ERRORS AND ASSOCIATED MESSAGES
+app.use(errorHandler);
+
+
+app.listen(PORT, () => {
+    console.log(`The dev^2 server is currently running on port ${PORT}!`);
+})
