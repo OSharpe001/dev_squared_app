c1d2d0531f695e856874d13d8488f4e9a16d4e9d
O. Sharpe <osharpe001@gmail.com>
2023-09-09 14:10:30 -0400

    added corsOption to help avoid cors issues

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,15 @@ app.use("/api/comments", require("./routes/commentRoutes"));
 // USE ROUTES/LIKESROUTES TO HANDLE ANY ENDPOINTS THAT END WITH /API/LIKES
 // app.use("/api/likes", require("./routes/likesRoutes"));
 
+// SETTING UP CORS OPTIONS TO AVOID ACCESS-CONTROL-ALLOW-ORIGIN ISSUES
+const corsOption = {
+    origin: "*",
+    methods: "GET, PUT, POST, DELETE",
+    credentials: true,
+    exposeHeaders: ["X-auth-token"],
+}
 // TRYING TO AVOID THE ACCESS-CONTROL-ALLOW-ORIGIN ISSUE
-app.use(cors());
+app.use(cors(corsOption));
 
 // HANDLING ERRORS AND ASSOCIATED MESSAGES
 app.use(errorHandler);
