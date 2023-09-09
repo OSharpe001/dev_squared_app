570777de46f85ccbdb913282a020387cb03c99a3
O. Sharpe <osharpe001@gmail.com>
2023-09-09 01:17:13 -0400

    fixed updateBlog and setBlogs error msg

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -22,7 +22,7 @@ const setBlogs = asyncHandler(async (req, res) => {
 
     if (!req.body.text) {
         res.status(400);
-        throw new Error("Please add your blog'scontent...");
+        throw new Error("Please add your blog's content...");
     };
 
     const blog = await Blog.create({
@@ -51,6 +51,12 @@ const updateBlogs = asyncHandler(async (req,res) => {
         throw new Error("User not found...");
     };
 
+    // CHECK TO SEE IF AN ITEM WAS CHANGED
+    if ((req.body.title == blog.title && req.body.text === blog.text) || (!req.body.title && !req.body.text) || (req.body.title == blog.title && !req.body.text) || (req.body.text === blog.text && !req.body.title)) {
+        res.status(401);
+        throw new Error("Please change at least one field...")
+    };
+
     // MAKE SURE THE CURRENT USER MATCHES THE BLOG'S CREATOR
     if (blog.user.toString() !== req.user.id) {
         res.status(401);
@@ -58,7 +64,7 @@ const updateBlogs = asyncHandler(async (req,res) => {
     };
 
     // FINDING THE BLOG AND UPDATING IT
-    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body.title || blog.title, req.body.text || blog.text, { new: true });
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, {new: true});
 
     res.status(200).json(updatedBlog);
 });
