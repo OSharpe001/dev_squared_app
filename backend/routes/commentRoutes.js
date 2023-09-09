726e700e88e245c5f42394a7320564d05f979418
O. Sharpe <osharpe001@gmail.com>
2023-09-09 19:43:00 -0400

    added getBlogComments, getUserComments routes

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getComments, setComments, updateComments, deleteComments } = require("../controllers/commentController");
+const { getBlogComments, getUserComments, setComments, updateComments, deleteComments } = require("../controllers/commentController");
 
 const { protect } = require("../middleware/authMiddleware");
 
 // COMMENT ROUTES--
 // (CHAINING METHODS THAT HAVE THE SAME ROUTE)
-router.route("/").get(getComments).post(protect, setComments);
+router.route("/").get(getBlogComments).get(getUserComments).post(protect, setComments);
 
 router.route("/:id").put(protect, updateComments).delete(protect, deleteComments);
 
