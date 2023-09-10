ab5ce68f280dc4ba972284dd0341d5c426ac5789
O. Sharpe <osharpe001@gmail.com>
2023-09-10 05:49:03 -0400

    made spinner component (Brad Traversy's)

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Spinner() {
+    return (
+        <div className='loadingSpinnerContainer'>
+            <div className='loadingSpinner'></div>
+        </div>
+    );
+};
