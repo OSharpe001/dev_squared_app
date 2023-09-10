f0a82f28fc2601ed25ac6d8f7460ae5d8058b406
O. Sharpe <osharpe001@gmail.com>
2023-09-10 00:01:54 -0400

    started working on Header (navbar)

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,79 @@
+// import logo from "../assets/images/logo.png";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+
+
+export default function Header({ navigate, loggedIn, setLoggedIn }) {
+
+  const backHome = () => {
+    navigate("/");
+  };
+
+  const logOut = () => {
+    setLoggedIn(false);
+    navigate("/sign-up")
+  }
+
+  // NECESSARY TO CHANGE THE "VIEW-POSTER" BUTTON TO "CHANGE-POSTER" WHEN ON POSTER PAGE
+  const currentScreen = useLocation().pathname;
+
+  // **SHIFTING HEADER USESTATES AND FUNCTIONS
+  const [scrollPosition, setScrollPosition] = useState({
+    y: 0,
+  });
+  const [prevScrollPosition, setPreviousScrollPositiion] = useState({
+    y: 0,
+  })
+  const handleScrollPositionChange = (e) => {
+    setPreviousScrollPositiion({
+      y: scrollPosition.y
+      },);
+    setScrollPosition({
+      y: window.scrollY
+    },);
+  };
+  window.addEventListener("scroll", handleScrollPositionChange);
+  let headerPos =  scrollPosition.y>0 && scrollPosition.y>prevScrollPosition.y ?"hide": "show";
+
+  return (
+    <header
+            className="nav-header"
+            style={
+              headerPos === "hide" ?
+                {
+                  position: "sticky",
+                  top: "-100px",
+                  animationName: "raise-nav"
+                }
+              :
+              {
+                position: "sticky",
+                top: "0",
+                animationName: "drop-nav"
+              }
+            }
+    >
+      <nav>
+        <div className="banner" onClick={backHome}>
+          {/* <img className="logo" src={logo} alt="target" /> */}
+          <p className="banner-title">Dev^2</p>
+        </div>
+        <div className="nav-items">
+            {
+                currentScreen === "/blog" &&
+                    <Link aria-label="On Click" to="/" className="button">Back to Blogs</Link>
+            }
+            {loggedIn ?
+                <Link onClick={logOut} className="button">Logout</Link>
+            :
+                currentScreen === "/sign-in" ?
+                    <Link aria-label="On Click" to="/sign-up" className="button">Register</Link>
+                :
+                    <Link aria-label="On Click" to="/sign-in" className="button">LogIn</Link>
+            }
+        </div>
+      </nav>
+    </header>
+  );
+};
