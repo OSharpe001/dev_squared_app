208b753f2497cb2ad862e50f4e6ca6a381f5d32f
O. Sharpe <osharpe001@gmail.com>
2023-09-11 13:32:10 -0400

    started CurrentBlogPage functions/useStates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,35 @@ import "react-toastify/dist/ReactToastify.css";
 import { CurrentBlog, HomePage, Login, Register } from "./pages";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 export default function App() {
 
-  const [loggedIn, setLoggedIn] = useState("");
+  const [loggedIn, setLoggedIn] = useState(JSON.parse(localStorage.getItem("Dev2User")));
+  const [currentBlog, setCurrentBlog] = useState("");
+  const [blogId, setBlogId] = useState("");
   const navigate = useNavigate();
+  console.log("APP'S CURRENT BLOGID VALUE: ", blogId);
+
+  useEffect(() => {
+    const getCurrentBlog = async () => {
+      if (blogId) {
+        const URL = `http://localhost:5011/api/blogs/${blogId}`;
+        const response = await axios.get(URL);
+        console.log("APP.JS' GETCURRENTBLOG VALUE: ", response.data);
+        try {
+          await setCurrentBlog(response.data);
+        } catch (err) {
+          alert("an Error occurred. Please try, again.")
+          console.log(err);
+        };
+        navigate("/blog");
+      };
+    };
+    getCurrentBlog();
+  }, [blogId, loggedIn, navigate]);
 
   return (
     <>
@@ -18,6 +40,8 @@ export default function App() {
         navigate={navigate}
         loggedIn={loggedIn}
         setLoggedIn={setLoggedIn}
+        setBlogId={setBlogId}
+        setCurrentBlog={setCurrentBlog}
       />
       <Routes>
         <Route path="/sign-in" element={<Login
@@ -30,12 +54,14 @@ export default function App() {
                                           navigate={navigate}
                                         />}/>
         <Route path="/blog" element={<CurrentBlog
+                                        currentBlog={currentBlog}
                                         navigate={navigate}
                                         loggedIn={loggedIn}
                                       />}/>
         <Route path="/" element={<HomePage
                                     navigate={navigate}
                                     loggedIn={loggedIn}
+                                    setBlogId={setBlogId}
                                   />}/>
       </Routes>
       <ToastContainer />
