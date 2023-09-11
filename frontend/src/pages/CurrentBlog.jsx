365df2340b0e242d316a3a21b66cdcb6b7a4bea9
O. Sharpe <osharpe001@gmail.com>
2023-09-11 13:38:26 -0400

    started working on CurrentBlog page

--- frontend/src/pages/CurrentBlog.jsx
+++ frontend/src/pages/CurrentBlog.jsx
@@ -1,15 +1,24 @@
 // import React from 'react';
 
 
-export default function CurrentBlog({ loggedIn, navigate }) {
+export default function CurrentBlog({ currentBlog ,loggedIn, navigate }) {
 
     if (!loggedIn) {
         navigate("/sign-up")
     };
 
+    console.log("CURRENTBLOGS CURRENTBLOG VALUE: ", currentBlog)
+
     return (
-        <div className="current-blog-page">
-            <h1 className="title">CurrentBlog</h1>
-        </div>
+        <section className="current-blog-page">
+            <div className="blog-info">
+                <h1 className="title">{currentBlog.title}</h1>
+                <p className="current-blog-author">By: {currentBlog.user}</p>
+                <p className="current-blog-date">{new Date(currentBlog.updatedAt).toLocaleString().split(",")[0]}</p>
+            </div>
+
+            <section className="blog-text">{currentBlog.text}</section>
+            
+        </section>
     );
 };
