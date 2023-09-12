8cb1c9dcd96cd82e7d33cfc07b9b4089d20cc702
O. Sharpe <osharpe001@gmail.com>
2023-09-12 14:18:40 -0400

    added updating-comment function

--- frontend/src/components/CommentModal.jsx
+++ frontend/src/components/CommentModal.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
-export default function CommentModal({ commentModalHidden, setCommentModalHidden, currentBlog, loggedIn }) {
+export default function CommentModal({ commentModalHidden, setCommentModalHidden, currentBlog, loggedIn, commentToUpdate }) {
 
     const [commentText, setCommentText] = useState("");
     const [formData, setFormData] = useState({
@@ -22,37 +22,61 @@ export default function CommentModal({ commentModalHidden, setCommentModalHidden
     // console.log("COMMENTMODAL'S COMMENTTEXT VALUE: ", commentText);
 
     const submitForm = () => {
-        setFormData(prev => ({
-            ...prev,
-            text: commentText
-        }));
-        console.log("COMMENTMODAL'S SUBMITFORM'S SETFORMDATA TEXT VALUE: ", commentText);
+            setFormData(prev => ({
+                ...prev,
+                text: commentText
+            }));
+        // console.log("COMMENTMODAL'S SUBMITFORM'S SETFORMDATA TEXT VALUE: ", commentText);
     };
 
     useEffect(() => {
-        const URL = "http://localhost:5011/api/blogs/comments";
+        if (commentToUpdate.text) {
+            setCommentText(commentToUpdate.text);
 
-        const createComment = async () => {
-            const config = {
-                headers: {
-                    Authorization: `Bearer ${loggedIn.token}`
-                },
+            if (formData.text) {
+                const updateComment = async () => {
+                    const URL = `http://localhost:5011/api/blogs/comments/${commentToUpdate.id}`;
+                    const config = {
+                        headers: {
+                            Authorization: `Bearer ${loggedIn.token}`
+                        },
+                    };
+                    try {
+                        const response = await axios.put(URL, formData, config);
+                        console.log("COMMENTMODAL'S USEEFFECT POST ATTEMPT RESPONSE: ", response);
+                    } catch (err) {
+                        console.log("FETCH ERROR: ", err)
+                    };
+                };
+                updateComment();
             };
-            try {
-                const response = await axios.post(URL, formData, config);
-                console.log("COMMENTMODAL'S USEEFFECT POST ATTEMPT RESPONSE: ", response);
-            } catch (err) {
-                console.log("FETCH ERROR: ", err)
+
+        } else {
+            const createComment = async () => {
+                const URL = "http://localhost:5011/api/blogs/comments";
+                const config = {
+                    headers: {
+                        Authorization: `Bearer ${loggedIn.token}`
+                    },
+                };
+                try {
+                    await axios.post(URL, formData, config);
+                } catch (err) {
+                    console.log("FETCH ERROR: ", err)
+                };
             };
+            createComment();
         };
-        createComment();
-    }, [formData, loggedIn.token]);
 
-  return (
-    <form className={commentModalHidden? "hidden": "modal"}>
-        <textarea className="modal-comment" value={commentText} onChange={handleTextChange} placeholder="Leave a comment..."/>
-        <button className="submit-text" onClick={submitForm}>Submit</button>
-        <button className="cancel" onClick={cancelComment}>Cancel</button>
-    </form>
-  );
+    }, [formData, loggedIn.token, currentBlog, commentToUpdate]);
+
+    // console.log("TEXT TO BE UPDATED: ", text, "CURRENT COMMENTTEXT: ", commentText);
+
+    return (
+        <form className={commentModalHidden ? "hidden" : "modal"}>
+            <textarea className="modal-comment" value={commentText} onChange={handleTextChange} placeholder="Leave a comment..." />
+            <button className="submit-text" onClick={submitForm}>Submit</button>
+            <button className="cancel" onClick={cancelComment}>Cancel</button>
+        </form>
+    );
 };
