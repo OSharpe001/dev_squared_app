1c9534fa877d138c4ca2d7df42d3e1c3634eb1ea
O. Sharpe <osharpe001@gmail.com>
2023-09-12 01:52:05 -0400

    changed the postComment function

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -15,7 +15,7 @@ const getUserComments = asyncHandler(async (req, res) => {
     res.status(200).json(comments);
 });
 
-// SET COMMENTS (POST REQUEST - "/api/blog_id/comments")
+// SET COMMENTS (POST REQUEST - "/api/blogs/comments")
 const setComments = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400);
@@ -23,8 +23,9 @@ const setComments = asyncHandler(async (req, res) => {
     };
     const comment = await Comment.create({
         text: req.body.text,
-        user: req.user.id,
-        // blog: req.blog.id,
+        userName: req.body.userName,
+        blogId: req.body.blogId,
+        user: req.user.id
     });
 
     res.status(200).json(comment);
@@ -102,3 +103,43 @@ const deleteComments = asyncHandler(async (req, res) => {
 
 
 module.exports = { getBlogComments, getUserComments, setComments, updateComments, deleteComments };
+
+
+
+// *****MY ATTEMPT TO MAKE A NEW "UPDATE COMMENTS" *********WILL HAVE TO TRY, LATER...
+// // UPDATE COMMENTS (PUT REQUEST - "/api/blog_id/comments/:id")
+// const updateComments = asyncHandler(async (req, res) => {
+//     // FIND THE COMMENT'S ID
+//     const comment = await Comment.findById(req.body.text);
+//     const creator = await Comment.findById(req.body.userName);
+//     const blogId = await Comment.findById(req.body.blogId);
+
+//     // CHECK IF THE GOAL EXISTS TO BE UPDATED
+//     if (!comment) {
+//         res.status(400);
+//         throw new Error("Comment does not exist...");
+//     };
+
+//     // CHECK IF USER EXISTS
+//     if (!creator) {
+//         res.status(401);
+//         throw new Error("Creator not found...");
+//     };
+
+//     // CHECK IF BLOG EXISTS
+//     if (!blogId) {
+//         res.status(401);
+//         throw new Error("Associated blog not found...");
+//     };
+
+//     // MAKE SURE THE CURRENT USER WROTE THE COMMENT
+//     if (comment.user.toString() !== req.user.id) {
+//         res.status(401);
+//         throw new Error("Not authorized...");
+//     };
+
+//     // FINDING THE COMMENT (IF IT EXISTS) AND UPDATING IT
+//     const updatedComment = await Comment.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+//     res.status(200).json(updatedComment);
+// });
