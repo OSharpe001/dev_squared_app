9ca3a1ad7e6a5d4472afb23013fc93cb7b4aa6da
O. Sharpe <osharpe001@gmail.com>
2023-09-12 01:54:13 -0400

    added CommentModal. still needs work

--- frontend/src/pages/CurrentBlog.jsx
+++ frontend/src/pages/CurrentBlog.jsx
@@ -1,23 +1,64 @@
-// import React from 'react';
+import { useState, useEffect } from 'react';
+import CommentModal from '../components/CommentModal';
 
 
-export default function CurrentBlog({ currentBlog ,loggedIn, navigate }) {
+export default function CurrentBlog({ currentBlog , loggedIn, navigate }) {
 
-    if (!loggedIn) {
-        navigate("/sign-up")
+    const [commentModalHidden, setCommentModalHidden] = useState(true);
+
+    useEffect(() => {
+        if (!loggedIn) {
+            navigate("/sign-up");
+        };
+    
+        if (loggedIn && !currentBlog) {
+            navigate("/");
+        };
+    
+      
+    }, [loggedIn, currentBlog, navigate])
+    
+    
+
+    const startComment =() => {
+        setCommentModalHidden(false);
     };
 
-    console.log("CURRENTBLOGS CURRENTBLOG VALUE: ", currentBlog)
+    console.log("**CURRENTBLOGS CURRENTBLOG VALUE: ", currentBlog);
+    console.log("**CURRENTBLOGS LOGGEDIN INFO: ", loggedIn);
 
     return (
         <section className="current-blog-page">
-            <div className="blog-info">
-                <h1 className="title">{currentBlog.title}</h1>
-                <p className="current-blog-author">By: {currentBlog.user}</p>
-                <p className="current-blog-date">{new Date(currentBlog.updatedAt).toLocaleString().split(",")[0]}</p>
+            <div className="blog">
+                <h2 className="blog-title underlined">{currentBlog.title}</h2>
+                <p className="blog-text">{currentBlog.text}</p>
+                <div className="blog-info">
+                    <p className="current-blog-author underlined">By: {currentBlog.user}</p>
+                    <p className="current-blog-date underlined">{new Date(currentBlog.updatedAt).toLocaleString().split(",")[0]}</p>
+                </div>
             </div>
 
-            <section className="blog-text">{currentBlog.text}</section>
+            <div className="add-or-update">
+            {loggedIn._id === currentBlog.user ?
+                <>
+                    <button>Delete</button>
+                    <button>Edit</button>
+                    <button onClick={startComment}>Comment</button>
+                </>
+            :
+                <>
+                    <button>Like</button>
+                    <button>Comment</button>
+                </>
+            }
+            </div>
+
+            <CommentModal
+                        commentModalHidden={commentModalHidden}
+                        setCommentModalHidden={setCommentModalHidden}
+                        loggedIn={loggedIn}
+                        currentBlog={currentBlog}
+                    />
             
         </section>
     );
