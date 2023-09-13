c10f6a479af6f94b63cea67de542a75d05d31713
O. Sharpe <osharpe001@gmail.com>
2023-09-12 20:59:57 -0400

    set blogModal to create blogs and for styles

--- frontend/src/components/BlogModal.jsx
+++ frontend/src/components/BlogModal.jsx
@@ -1,20 +1,24 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
-export default function BlogModal({ blogModalHidden, setBlogModalHidden, loggedIn }) {
+export default function BlogModal({ blogModalHidden, setBlogModalHidden, loggedIn, currentBlog }) {
+
+    const [blogTitle, setBlogTitle] = useState("");
+    const [blogText, setBlogText] = useState("");
 
     const [formData, setFormData] = useState({
         blogTitle: "",
         blogText: "",
         userName: loggedIn.userName
     });
-    // const [blogText, setBlogText] = useState("");
 
-    const handleChange = ({ target }) => {
-        setFormData(prev => ({
-            ...prev,
-            [target.name]: target.value
-        }));
+    const handleTitleChange = ({ target }) => {
+        setBlogTitle(target.value);
+    };
+
+    const handleTextChange = ({ target }) => {
+        setBlogText(target.value);
     };
 
     const cancelBlog = () => {
@@ -26,12 +30,40 @@ export default function BlogModal({ blogModalHidden, setBlogModalHidden, loggedI
         setBlogModalHidden(true);
     };
 
-  return (
-    <form className={blogModalHidden? "hidden": "modal"}>
-        <input type="text" className="modal-title" onChange={handleChange}value={formData.blogTitle} placeholder="leave a comment"/>
-        <input type="text" className="modal-text" onChange={handleChange}value={formData.blogText} placeholder="leave a comment"/>
-        <button className="submit-text" >submit</button>
-        <button className="cancel" onClick={cancelBlog}>Cancel</button>
-    </form>
-  );
+    const submitForm = () => {
+        setFormData(prev => ({
+            ...prev,
+            title: blogTitle,
+            text: blogText
+        }));
+        console.log(`BLOGMODAL'S SUBMITFORM'S SETFORMDATA TEXT AND TITLE VALUE: TITLE- ${blogTitle} TEXT- ${blogText}`);
+    };
+
+    useEffect(() => {
+        if (formData.title && formData.text) {
+            const createBlog = async () => {
+                const URL = "http://localhost:5011/api/blogs/";
+                const config = {
+                    headers: {
+                        Authorization: `Bearer ${loggedIn.token}`
+                    },
+                };
+                try {
+                    await axios.post(URL, formData, config);
+                } catch (err) {
+                    console.log("FETCH ERROR: ", err);
+                };
+            };
+            createBlog();
+        };
+    }, [formData, loggedIn]);
+
+    return (
+        <form className={blogModalHidden ? "hidden" : "blog-modal modal"}>
+            <input type="text" className="modal-title" onChange={handleTitleChange} value={blogTitle} placeholder="Title" focused/>
+            <textarea className="modal-blog" onChange={handleTextChange} value={blogText} placeholder="What are your thoughts" />
+            <button className="submit-text" onClick={submitForm}>submit</button>
+            <button className="cancel" onClick={cancelBlog}>Cancel</button>
+        </form>
+    );
 };
