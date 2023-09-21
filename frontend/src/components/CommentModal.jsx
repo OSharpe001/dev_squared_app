a226eb90be9ef8504ddae56389e2ad96a93018c0
O. Sharpe <osharpe001@gmail.com>
2023-09-21 16:27:33 -0400

    added a new comment and tested a few more options

--- frontend/src/components/CommentModal.jsx
+++ frontend/src/components/CommentModal.jsx
@@ -29,6 +29,7 @@ export default function CommentModal({ commentModalHidden, setCommentModalHidden
 
     useEffect(() => {
         if (commentToUpdate.text) {
+            // REFILLS COMMENT MODAL WITH THE PREVIOUS INFO OF THE COMMENT THAT NEEDS UPDATING
             setCommentText(commentToUpdate.text);
             // setBlogId(formData.blogId); // RESETTING THE "CURRENT BLOG ID" BEFORE THE COMMENT UPDATE DIDN'T WORK (TO MAKE THE PAGE REDIRECT BACK TO THE CURRENT BLOG)
 
@@ -44,6 +45,8 @@ export default function CommentModal({ commentModalHidden, setCommentModalHidden
                     };
                     try {
                         await axios.put(URL, formData, config);
+                        // setTimeout(setBlogId, 1100, formData.blogId); // PUTTING SETBLOGID ON A TIMEOUT WITHIN THE UPDATECOMMENT FUNCTION DIDN'T WORK
+                        // setTimeout(console.log, 1100, "COMMENTMODAL DURING-UPDATECOMMENT FORMDATA.BLOGID: ", formData.blogId);
                         // setBlogId(formData.blogId); // RESETTING THE "CURRENT BLOG ID" DURING THE COMMENT UPDATE DIDN'T WORK (TO MAKE THE PAGE REDIRECT BACK TO THE CURRENT BLOG)
                         // navigate("/blog"); // TRYING TO FORCE A REDIRECTION BACK TO THE CURRENT BLOG ONLY GETS REDIRECTED BACK SINCE DURING THE RE-RENDERS, THE BLOG.ID IS LOST (AUTO-HOME-RERENDER FUNCTION ON APP.JSX)
                     } catch (err) {
@@ -51,8 +54,8 @@ export default function CommentModal({ commentModalHidden, setCommentModalHidden
                     };
                 };
                 updateComment();
-                setTimeout(setBlogId, 100, formData.blogId); // setBlogId(formData.blogId); // RESETTING THE "CURRENT BLOG ID" AFTER THE COMMENT UPDATE DIDN'T WORK (TO MAKE THE PAGE REDIRECT BACK TO THE CURRENT BLOG)
-                console.log("COMMENTMODAL AFTER-UPDATECOMMENT FORMDATA.BLOGID: ", formData.blogId);
+                // setTimeout(setBlogId, 1100, formData.blogId); // setBlogId(formData.blogId); // RESETTING THE "CURRENT BLOG ID" AFTER THE COMMENT UPDATE DIDN'T WORK (TO MAKE THE PAGE REDIRECT BACK TO THE CURRENT BLOG)
+                // console.log("COMMENTMODAL AFTER-UPDATECOMMENT FORMDATA.BLOGID: ", formData.blogId); // BY THE TIME THAT THIS RUNS, FORMDATA.BLOGID IS EMPTY....
                 // navigate("/blog"); // TRYING TO FORCE A REDIRECTION BACK TO THE CURRENT BLOG ONLY GETS REDIRECTED BACK SINCE DURING THE RE-RENDERS, THE BLOG.ID IS LOST (AUTO-HOME-RERENDER FUNCTION ON APP.JSX)
             };
 
@@ -84,7 +87,7 @@ export default function CommentModal({ commentModalHidden, setCommentModalHidden
         // setBlogId(formData.blogId); // RESETTING THE "CURRENT BLOG ID" AFTER THE COMMENT CREATE/DELETE FUNCTIONS DIDN'T WORK (TO MAKE THE PAGE REDIRECT BACK TO THE CURRENT BLOG)
         // navigate("/blog"); // TRYING TO FORCE A REDIRECTION BACK TO THE CURRENT BLOG ONLY GETS REDIRECTED BACK SINCE DURING THE RE-RENDERS, THE BLOG.ID IS LOST (AUTO-HOME-RERENDER FUNCTION ON APP.JSX)
 
-    }, [formData, commentToUpdate/*, loggedIn.token, currentBlog, navigate,, setBlogId*/]); // CURRENTLY DON'T NEED THE COMMENTED OUT  PARAMETERS TO OPERATE THIS USEEFFECT...
+    }, [formData, commentToUpdate/*, loggedIn.token, currentBlog, navigate,, setBlogId*/]); // CURRENTLY DON'T NEED THE COMMENTED OUT PARAMETERS TO OPERATE THIS USEEFFECT...
 
     return (
         <form className={commentModalHidden ? "hidden" : "comment-modal modal"}>
