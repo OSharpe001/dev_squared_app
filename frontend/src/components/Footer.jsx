24f9a4ed5711774efff414ca702b577d1cbb5b1f
O. Sharpe <osharpe001@gmail.com>
2023-09-23 20:10:27 -0400

    placed more into Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
 
   return (
     <footer>
-      <p>Dev^2©    2023</p>
+      <p>Dev^2© • Dev Squared© • 2023</p>
       <nav>
         <Link className="nav-item button" aria-label="On Click" target="_blank" to="https://osharpesportfolio.netlify.app" >O. Sharpe's Portfolio</Link>
       </nav>
