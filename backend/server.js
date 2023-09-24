8949f12c011bb4e97ed5036c0277639b14d57c82
O. Sharpe <osharpe001@gmail.com>
2023-09-23 23:07:56 -0400

    Fixed CORS issue

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,16 @@ const app = express();
 connectDB();
 
 // MIDDLEWARE-- //
+// SETTING UP CORS OPTIONS TO AVOID ACCESS-CONTROL-ALLOW-ORIGIN ISSUES **THIS MUST BE PLACE ABOVE ALL OTHER MIDDLEWARE FUNCTIONS!**
+const corsOption = {
+    origin: "*",
+    methods: "GET, PUT, POST, DELETE",
+    credentials: true,
+    exposeHeaders: ["X-auth-token"],
+};
+// TO AVOID THE ACCESS-CONTROL-ALLOW-ORIGIN ISSUE
+app.use(cors(corsOption));
+
 // TO ALLOW THE BODY OF A POST REQUEST
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,16 +31,6 @@ app.use("/api/blogs", require("./routes/blogRoutes"));
 app.use("/api/blogs/comments", require("./routes/commentRoutes"));
 // USE ROUTES/LIKESROUTES TO HANDLE ANY ENDPOINTS THAT END WITH /API/LIKES
 app.use("/api/likes", require("./routes/likesRoutes"));
-
-// SETTING UP CORS OPTIONS TO AVOID ACCESS-CONTROL-ALLOW-ORIGIN ISSUES
-const corsOption = {
-    origin: "*",
-    methods: "GET, PUT, POST, DELETE",
-    credentials: true,
-    exposeHeaders: ["X-auth-token"],
-};
-// TRYING TO AVOID THE ACCESS-CONTROL-ALLOW-ORIGIN ISSUE
-app.use(cors(corsOption));
 
 // HANDLING ERRORS AND ASSOCIATED MESSAGES
 app.use(errorHandler);
