e0bf7105f8cef53054d2764d3e6413d40e3aacd5
O. Sharpe <osharpe001@gmail.com>
2023-09-27 00:44:29 -0400

    slight clean up

--- frontend/src/components/CommentModal.jsx
+++ frontend/src/components/CommentModal.jsx
@@ -61,7 +61,7 @@ export default function CommentModal({ commentModalHidden, setCommentModalHidden
         } else if (formData.text) {
             // console.log("14");
             const createComment = async () => {
-                const URL = "https://devsquaredbe.onrender.com/api/blogs/comments";
+                const URL = "https://devsquaredbe.onrender.com/api/blogs/comments/";
                 // const URL = "http://localhost:5011/api/blogs/comments";
                 const config = {
                     headers: {
