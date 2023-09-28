c9e346ba252a792cd9779f6733bfb9e23a0ae056
O. Sharpe <osharpe001@gmail.com>
2023-09-28 00:10:26 -0400

    changes to headers in server

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
      */
     res.setHeader('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-auth-token');
-    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Accept');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Accept');
     // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
