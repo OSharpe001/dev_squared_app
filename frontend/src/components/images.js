bb26bd966f9f68d1fa86989e8ae24f806e9fefe3
O. Sharpe <osharpe001@gmail.com>
2023-09-29 23:23:52 -0400

    more memes

--- frontend/src/components/images.js
+++ frontend/src/components/images.js
@@ -26,6 +26,72 @@ import pic25 from "../assets/images/memes/IMG_8210.jpg";
 import pic26 from "../assets/images/memes/IMG_8275.jpg";
 import pic27 from "../assets/images/memes/IMG_8287.jpg";
 import pic28 from "../assets/images/memes/IMG_8288.jpg";
+import pic29 from "../assets/images/memes/IMG_8301.jpg";
+import pic30 from "../assets/images/memes/IMG_8302.jpg";
+import pic31 from "../assets/images/memes/IMG_8303.jpg";
+import pic32 from "../assets/images/memes/IMG_8304.jpg";
+import pic33 from "../assets/images/memes/IMG_8305.jpg";
+import pic34 from "../assets/images/memes/IMG_8306.jpg";
+import pic35 from "../assets/images/memes/IMG_8307.jpg";
+import pic36 from "../assets/images/memes/IMG_8308.jpg";
+import pic37 from "../assets/images/memes/IMG_8309.jpg";
+import pic38 from "../assets/images/memes/IMG_8311.jpg";
+import pic39 from "../assets/images/memes/IMG_8312.jpg";
+import pic40 from "../assets/images/memes/IMG_8313.jpg";
+import pic41 from "../assets/images/memes/IMG_8314.jpg";
+import pic42 from "../assets/images/memes/IMG_8315.jpg";
+import pic43 from "../assets/images/memes/IMG_8318.jpg";
+import pic44 from "../assets/images/memes/IMG_8319.jpg";
+import pic45 from "../assets/images/memes/IMG_8320.jpg";
+import pic46 from "../assets/images/memes/IMG_8322.jpg";
+import pic47 from "../assets/images/memes/IMG_8324.jpg";
+import pic48 from "../assets/images/memes/IMG_8326.jpg";
+import pic49 from "../assets/images/memes/IMG_8327.jpg";
+import pic50 from "../assets/images/memes/IMG_8328.jpg";
+import pic51 from "../assets/images/memes/IMG_8329.jpg";
+import pic52 from "../assets/images/memes/IMG_8331.jpg";
+import pic53 from "../assets/images/memes/IMG_8332.jpg";
+import pic54 from "../assets/images/memes/IMG_8339.jpg";
+import pic55 from "../assets/images/memes/IMG_8340.jpg";
+import pic56 from "../assets/images/memes/IMG_8341.jpg";
+import pic57 from "../assets/images/memes/IMG_8344.jpg";
+import pic58 from "../assets/images/memes/IMG_8345.jpg";
+import pic59 from "../assets/images/memes/IMG_8347.jpg";
+import pic60 from "../assets/images/memes/IMG_8348.jpg";
+import pic61 from "../assets/images/memes/IMG_8349.jpg";
+import pic62 from "../assets/images/memes/IMG_8350.jpg";
+import pic63 from "../assets/images/memes/IMG_8351.jpg";
+import pic64 from "../assets/images/memes/IMG_8352.jpg";
+import pic65 from "../assets/images/memes/IMG_8353.jpg";
+import pic66 from "../assets/images/memes/IMG_8354.jpg";
+import pic67 from "../assets/images/memes/IMG_8355.jpg";
+import pic68 from "../assets/images/memes/IMG_8357.jpg";
+import pic69 from "../assets/images/memes/IMG_8360.jpg";
+import pic70 from "../assets/images/memes/IMG_8361.jpg";
+import pic71 from "../assets/images/memes/IMG_8362.jpg";
+import pic72 from "../assets/images/memes/IMG_8363.jpg";
+import pic73 from "../assets/images/memes/IMG_8364.jpg";
+import pic74 from "../assets/images/memes/IMG_8365.jpg";
+import pic75 from "../assets/images/memes/IMG_8366.jpg";
+import pic76 from "../assets/images/memes/IMG_8368.jpg";
+import pic77 from "../assets/images/memes/IMG_8370.jpg";
+import pic78 from "../assets/images/memes/IMG_8373.jpg";
+import pic79 from "../assets/images/memes/IMG_8374.jpg";
+import pic80 from "../assets/images/memes/IMG_8375.jpg";
+import pic81 from "../assets/images/memes/IMG_8376.jpg";
+import pic82 from "../assets/images/memes/IMG_8378.jpg";
+import pic83 from "../assets/images/memes/IMG_8379.jpg";
+import pic84 from "../assets/images/memes/IMG_8380.jpg";
+import pic85 from "../assets/images/memes/IMG_8382.WEBP";
+import pic86 from "../assets/images/memes/IMG_8384.jpg";
+import pic87 from "../assets/images/memes/IMG_8385.WEBP";
+import pic88 from "../assets/images/memes/IMG_8386.WEBP";
+import pic89 from "../assets/images/memes/IMG_8388.WEBP";
+import pic90 from "../assets/images/memes/IMG_8389.jpg";
+import pic91 from "../assets/images/memes/IMG_8390.WEBP";
+import pic92 from "../assets/images/memes/IMG_8391.jpg";
+import pic93 from "../assets/images/memes/IMG_8392.WEBP";
+import pic94 from "../assets/images/memes/IMG_8393.WEBP";
 
 export const images = [
     {
@@ -166,6 +232,336 @@ export const images = [
     {
         class: "meme-image",
         image: pic28,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic29,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic30,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic31,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic32,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic33,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic34,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic35,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic36,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic37,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic38,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic39,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic40,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic41,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic42,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic43,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic44,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic45,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic46,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic47,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic48,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic49,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic50,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic51,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic52,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic53,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic54,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic55,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic56,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic57,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic58,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic59,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic60,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic61,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic62,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic63,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic64,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic65,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic66,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic67,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic68,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic69,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic70,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic71,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic72,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic73,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic74,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic75,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic76,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic77,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic78,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic79,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic80,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic81,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic82,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic83,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic84,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic85,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic86,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic87,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic88,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic89,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic90,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic91,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic92,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic93,
+        alt: "meme"
+    },
+    {
+        class: "meme-image",
+        image: pic94,
         alt: "meme"
     },
 ];
