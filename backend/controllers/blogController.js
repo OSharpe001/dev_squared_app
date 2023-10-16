efd21f43727cdfa24e7b7bfaf0b4658f556bebc4
O. Sharpe <osharpe001@gmail.com>
2023-10-15 23:37:43 -0400

    add function to delete associated likes/comments. deleteBlogs

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,5 +1,8 @@
 const asyncHandler = require("express-async-handler");
 const Blog = require("../models/blogModel");
+const Like = require("../models/likesModel");
+const Comment = require("../models/commentModel");
+const { findByIdAndRemove } = require("../models/userModel");
 
 // GET BLOGS LIST (GET REQUEST - "/api/blogs")
 const getBlogsList = asyncHandler(async (req, res) => {
@@ -17,12 +20,12 @@ const getBlog = asyncHandler(async (req, res) => {
 const setBlogs = asyncHandler(async (req, res) => {
     if (!req.body.title) {
         res.status(400);
-        throw new Error("Please add a title...");
+        throw new Error("Denied. No title.");
     };
 
     if (!req.body.text) {
         res.status(400);
-        throw new Error("Please add your blog's content...");
+        throw new Error("Denied. No content.");
     };
 
     const blog = await Blog.create({
@@ -36,36 +39,36 @@ const setBlogs = asyncHandler(async (req, res) => {
 });
 
 // UPDATE BLOGS (PUT REQUEST - "/api/blogs/:id")
-const updateBlogs = asyncHandler(async (req,res) => {
+const updateBlogs = asyncHandler(async (req, res) => {
     // FIND ID
     const blog = await Blog.findById(req.params.id);
 
     // CHECK IF THE BLOG EXISTS
     if (!blog) {
         res.status(400);
-        throw new Error("Blog not found...");
+        throw new Error("Blog not found.");
     };
 
     // CHECK FOR THE USER
     if (!req.user) {
         res.status(401);
-        throw new Error("User not found...");
+        throw new Error("User not found.");
     };
 
     // CHECK TO SEE IF AN ITEM WAS CHANGED
     if ((req.body.title == blog.title && req.body.text === blog.text) || (!req.body.title && !req.body.text) || (req.body.title == blog.title && !req.body.text) || (req.body.text === blog.text && !req.body.title)) {
         res.status(401);
-        throw new Error("No edits found...")
+        throw new Error("Denied. Not edited.");
     };
 
     // MAKE SURE THE CURRENT USER MATCHES THE BLOG'S CREATOR
     if (blog.user.toString() !== req.user.id) {
         res.status(401);
-        throw new Error("User not authorized...");
+        throw new Error("Not authorized.");
     };
 
     // FINDING THE BLOG AND UPDATING IT
-    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, { new: true });
 
     res.status(200).json(updatedBlog);
 });
@@ -74,25 +77,44 @@ const updateBlogs = asyncHandler(async (req,res) => {
 const deleteBlogs = asyncHandler(async (req, res) => {
     // FIND THE BLOG BY ID
     const blog = await Blog.findById(req.params.id);
+    const associatedLikes = await Like.find({blogId: req.params.id});
+    const associatedComments = await Comment.find({blogId: req.params.id});
+    const associatedCommentsLikes = associatedComments.map(comment => comment._id);
 
     // CHECK IF THE BLOG EXISTS
     if (!blog) {
         res.status(400);
-        throw new Error("Blog not found...");
+        throw new Error("Blog not found.");
     };
 
     // CHECK FOR THE USER
     if (!req.user) {
         res.status(401);
-        throw new Error("User not found...");
+        throw new Error("User not found.");
     };
 
     // MAKE SURE THE CURRENT USER IS THE BLOG'S CREATOR
     if (blog.user.toString() !== req.user.id) {
         res.status(401);
-        throw new Error("User not authorized...");
+        throw new Error("Not authorized.");
     };
 
+    // DELETE ALL LIKES ASSOICIATED WITH THIS BLOG
+    associatedLikes.forEach(async (like) => {
+        await Like.findOneAndRemove({ blogId: like.blogId });
+    });
+
+    // DELETE ALL COMMENTS ASSOCIATED WITH THIS BLOG
+    associatedComments.forEach(async (comment) => {
+        await Comment.findOneAndRemove({ blogId: comment.blogId });
+    });
+
+    // DELETE ALL LIKES THAT ARE ASSOCIATED WITH THE COMMENTS OF THIS BLOG
+    associatedCommentsLikes.forEach(async (commentId) => {
+        await Like.findOneAndRemove({ commentId: commentId });
+    });
+
+    // DELETE BLOG
     await Blog.findByIdAndRemove(req.params.id);
 
     res.status(200).json({ id: req.params.id });
